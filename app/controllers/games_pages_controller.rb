42f048bbb1d3de3c874e166619030eedb597d32c
Joshua Yee <joshyee16@gmail.com>
2023-11-30 17:30:23 -0800

    create poker hand route

--- app/controllers/games_pages_controller.rb
+++ app/controllers/games_pages_controller.rb
@@ -8,6 +8,9 @@ class GamesPagesController < ApplicationController
   end
 
   def random_hand
-    render json: {hi: "hello"}
+    value = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+    suit = ["diamonds", "clubs", "hearts", "spades"]
+    hand = ["#{value.sample} of #{suit.sample}, #{value.sample} of #{suit.sample}"] 
+    render json: {hand: hand}
   end
 end
